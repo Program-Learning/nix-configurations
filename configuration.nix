f68eee3ef2f53133242b1c98da0422f8a16e2587
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2023-06-12 15:06:44 +0800

    Separate boot config to configs/boot.nix

--- configuration.nix
+++ configuration.nix
@@ -8,12 +8,8 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./configs/boot.nix
     ];
-
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
