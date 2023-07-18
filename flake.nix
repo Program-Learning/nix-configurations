a819071ff0b9d006e93af17ec75d20986acc6747
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2023-07-19 00:08:38 +0800

    Flake snap

--- flake.nix
+++ flake.nix
@@ -1,9 +1,13 @@
 {
   description = "A very basic flake";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs,flake-utils }: {
+  inputs.nixpkgs.url = "github:DataEraserC/nixpkgs";
+  inputs.home-manager.url = "github:nix-community/home-manager";
 
+  outputs = { self, nixpkgs, home-manager }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix ];
+    };
   };
 }
