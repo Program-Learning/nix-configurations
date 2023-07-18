147c66eeea176aac46ffcc86b05a2578f9132c3b
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2023-07-18 23:30:58 +0800

    Snap

--- flake.nix
+++ flake.nix
@@ -1,11 +1,9 @@
 {
   description = "A very basic flake";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+  outputs = { self, nixpkgs,flake-utils }: {
 
   };
 }
